129e5508ae7ede47ca60b95689e920530b18a1a2
CoderRedwing <ajiteshru@gmail.com>
2023-02-19 23:41:44 +0530

    as

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:linkedin/screen/splash_scr.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+
+}
+
+
---
+++ lib/screen/splash_scr.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:linkedin/screen/login_scr.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigatehome();
+  }
+  _navigatehome()async{
+    await Future.delayed( const Duration(milliseconds: 5100),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+  }
+  @override
+  Widget build(BuildContext context) {
+    //final double screenHeight=MediaQuery.of(context).size.height;
+    //final double screenWidth=MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Center(
+        child: Container(
+
+
+          child: Image.asset('images.jpeg'),
+        ),
+      ),
+    );
+  }
+}
+
