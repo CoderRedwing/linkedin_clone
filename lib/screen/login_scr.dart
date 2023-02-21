6bc3c7550206f824ab79f0ba8617f5b30bc488d6
CoderRedwing <ajiteshru@gmail.com>
2023-02-21 16:47:25 +0530

    a

--- lib/screen/login_scr.dart
+++ lib/screen/login_scr.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin/screen/signup.dart';
 import 'package:linkedin/widgets/button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,25 +13,29 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       backgroundColor: Colors.white,
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const SizedBox(height: 200,),
              Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text('Linked',style: TextStyle(
-                  fontSize: 45,
+                 Text('Linked',style: TextStyle(
+                  fontSize: 42,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue,
+                  color: Colors.blue.shade700,
                 ),),
                 const SizedBox(width: 5,),
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
-                    color: Colors.blue,
+                    color: Colors.blue.shade700,
                   ),
                   alignment: Alignment.center,
                   height: 50,
@@ -50,60 +55,64 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(height: 5,),
             const Text('Join a trusted community of 800M professionals',style: TextStyle(fontSize: 16),),
-            Container(height: 200,),
-           Container(
-             alignment: Alignment.bottomCenter,
-             child: Column(
-               crossAxisAlignment:CrossAxisAlignment.end
-               ,
-               children: [
-                 GestureDetector(
-                   onTap: (){},
-                   child: Container(
-                     height: 38,
-                     width: 315,
-                     decoration: BoxDecoration(
-                         borderRadius: BorderRadius.circular(20),
+             const SizedBox(height: 270,),
+            Container(
+               alignment: Alignment.bottomCenter,
+              child: Column(
 
-                         color: Colors.blue.shade800),
-                     child: const Padding(
-                       padding: EdgeInsets.all(8.0),
-                       child: Text("Join now",textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,
-                           fontSize: 15,color: Colors.white),),
-                     ),
-                   ),
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const JoinNow()));
+                    },
+                    child: Container(
+                      height: 38,
+                      width: 315,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
 
-                 ),
-                 const SizedBox(height: 10,),
-                 RoundButton(title: 'Continue with Google', onTap: (){}),
-                 const SizedBox(height: 10,),
-                 RoundButton(title: 'Continue with Facebook', onTap: (){}),
-                 const SizedBox(height: 15,),
-                 GestureDetector(
-                   onTap: (){},
-                   child: Container(
-                     height: 38,
-                     width: 315,
-                     decoration: BoxDecoration(
-                         borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue.shade800),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Join now",textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,
+                            fontSize: 15,color: Colors.white),),
+                      ),
+                    ),
 
-                         color: Colors.white),
-                     child:  Padding(
-                       padding: const EdgeInsets.all(8.0),
-                       child: Text("Sign in",textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,
-                           fontSize: 15,color: Colors.blue.shade600),),
-                     ),
-                   ),
+                  ),
+                  const SizedBox(height: 10,),
+                  RoundButton(title: 'Continue with Google', onTap: (){}),
+                  const SizedBox(height: 10,),
+                  RoundButton(title: 'Continue with Facebook', onTap: (){}),
+                  const SizedBox(height: 15,),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      height: 38,
+                      width: 315,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
 
-                 ),
-               ],
-             ),
-           )
+                          color: Colors.white),
+                      child:  Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Sign in",textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,
+                            fontSize: 15,color: Colors.blue.shade600),),
+                      ),
+                    ),
+
+                  ),
+                ],
+              ),
+            ),
+
+
           ],
 
 
 
-      )
+      ),
+
 
     );
   }
---
+++ lib/screen/signup.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class JoinNow extends StatelessWidget {
+  const JoinNow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+       backgroundColor: Colors.white,
+        body:Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+
+           crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            const SizedBox(height: 70,),
+          Row(
+           
+
+            children: [
+              Text('Linked',style: TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue.shade700,
+              ),),
+              const SizedBox(width: 5,),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  color: Colors.blue.shade700,
+                ),
+                alignment: Alignment.center,
+                height: 25,
+                width: 25,
+                child: const Text('in',style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white,
+                ),),
+
+              ),
+
+
+
+            ],
+          ),
+              const SizedBox(height: 36,),
+              const Text('Add your name',style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Colors.black
+
+              ),),
+              const SizedBox(height: 30,),
+              TextFormField(
+
+                decoration:  const InputDecoration(
+                  hintText: 'First name*',
+
+
+                ),
+              ),
+              const SizedBox(height: 25,),
+              TextFormField(
+
+                decoration:  const InputDecoration(
+                  hintText: 'Last name*',
+                ),
+              ),
+              const SizedBox(height: 35,),
+              GestureDetector(
+                onTap: (){
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const JoinNow()));
+                },
+                child: Container(
+                  height: 38,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+
+                      color: Colors.blue.shade800),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Continue",textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,
+                        fontSize: 15,color: Colors.white),),
+                  ),
+                ),
+
+              ),
+            ]
+          ),
+        ) );
+
+  }
+}
+
